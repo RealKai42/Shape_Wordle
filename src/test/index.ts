b9c1a71a90be315a6b683d21396beebce3c17df7
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2020-08-10 16:32:05 +0800

    优化测试代码结构

--- src/test/index.ts
+++ src/test/index.ts
@@ -33,21 +33,25 @@ const { dist, group } = processImageData(distRaw, groupRaw, defaultOptions)
 const regions = processDistanceField(dist, contours, areas)
 allocateWords(keywords, regions, areas, options)
 
-// 可视化 or 打印信息
+// 展示文本预处理
 console.log(`------------------------------------------------------`)
 console.log("单词数量", words.length)
 for (let i = 0; i < 10; i++) {
   console.log(words[i])
 }
-
+// 可视化 distance field
 distanceVis(dist, options, outputDir)
+// 可视化分组信息
 groupVis(group, options, outputDir)
+// 可视化轮廓识别信息
 contourVis(contours, options, outputDir)
+// 可视化极点提取信息
 console.log(`------------------------------------------------------`)
 console.log("regions 信息")
 extremePointVis(dist, regions, options, outputDir)
+// 可视化单词分配到region和极点结果
 console.log(`------------------------------------------------------`)
 console.log("keywords 分配信息")
 allocateWordsVis(dist, regions, keywords, options, outputDir)
-
+// 可视化螺旋线生成效果
 spiralVis(dist, regions, options, outputDir)
